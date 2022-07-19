37d2232d4c8402c76cdb63f946bdee6aef1a1e1e
Frederik Rietdijk <fridh@fridh.nl>
2022-07-19 11:15:37 +0200

    ENH: test_signal: group tests using classes

--- tests/test_signal.py
+++ tests/test_signal.py
@@ -59,36 +59,34 @@ def signal(dask, nchannels):
     return signal
 
 
-@pytest.fixture(params=["full", "same"], ids=["full", "same"])
-def convolve_mode(request):
-    return request.param
+class TestConvolve:
+    @pytest.fixture(params=["full", "same"], ids=["full", "same"])
+    def convolve_mode(self, request):
+        return request.param
 
+    def test_fftconvolve(self, signal, convolve_mode):
 
-def test_fftconvolve(signal, convolve_mode):
+        result = fftconvolve(signal, signal, mode=convolve_mode)
+        if convolve_mode == "full":
+            assert len(result.time) == len(signal.time) + len(signal.time) - 1
+        elif convolve_mode == "same":
+            assert len(result.time) == len(signal.time)
 
-    result = fftconvolve(signal, signal, mode=convolve_mode)
-    if convolve_mode == "full":
-        assert len(result.time) == len(signal.time) + len(signal.time) - 1
-    elif convolve_mode == "same":
-        assert len(result.time) == len(signal.time)
+        assert set(signal.dims) == set(result.dims)
 
-    assert set(signal.dims) == set(result.dims)
+    @pytest.fixture(params=[["time"], None])
+    def dims(self, request):
+        return request.param
 
+    def test_fftconvolve(self, signal, convolve_mode, dims):
 
-@pytest.fixture(params=[["time"], None])
-def dims(request):
-    return request.param
+        result = fftconvolve(signal, signal, dims=dims, mode=convolve_mode)
+        if convolve_mode == "full":
+            assert len(result.time) == len(signal.time) + len(signal.time) - 1
+        elif convolve_mode == "same":
+            assert len(result.time) == len(signal.time)
 
-
-def test_fftconvolve(signal, convolve_mode, dims):
-
-    result = fftconvolve(signal, signal, dims=dims, mode=convolve_mode)
-    if convolve_mode == "full":
-        assert len(result.time) == len(signal.time) + len(signal.time) - 1
-    elif convolve_mode == "same":
-        assert len(result.time) == len(signal.time)
-
-    assert set(signal.dims) == set(result.dims)
+        assert set(signal.dims) == set(result.dims)
 
 
 def test_decimate(signal, nchannels):
@@ -103,226 +101,231 @@ def test_decimate(signal, nchannels):
         assert len(result.channel) == nchannels
 
 
-SIGNALS = {
-    # Horizontal is time, vertical is frequency
-    "1_signal_1_width": np.array(
+class TestPeakWidths:
+    @pytest.fixture
+    def signal(self, request):
+        SIGNALS = {
+            # Horizontal is time, vertical is frequency
+            "1_signal_1_width": np.array(
+                [
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [1, 0, 0, 0, 0, 0, 0],
+                    [0, 1, 0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0, 0, 0],
+                    [0, 0, 0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 0, 0, 1],
+                    [0, 0, 0, 0, 0, 0, 0],
+                ]
+            ),
+            "1_signal_2_width": np.array(
+                [
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [1, 0, 0, 0, 0, 0, 0],
+                    [1, 1, 0, 0, 0, 0, 0],
+                    [0, 1, 1, 0, 0, 0, 0],
+                    [0, 0, 1, 1, 0, 0, 0],
+                    [0, 0, 0, 1, 1, 0, 0],
+                    [0, 0, 0, 0, 1, 1, 0],
+                    [0, 0, 0, 0, 0, 1, 1],
+                    [0, 0, 0, 0, 0, 0, 1],
+                    [0, 0, 0, 0, 0, 0, 0],
+                ]
+            ),
+            "2_signal_1_width": np.array(
+                [
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [1, 0, 0, 0, 0, 0, 0],
+                    [0, 1, 0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0, 0, 0],
+                    [1, 0, 0, 1, 0, 0, 0],
+                    [0, 1, 0, 0, 1, 0, 0],
+                    [0, 0, 1, 0, 0, 1, 0],
+                    [0, 0, 0, 1, 0, 0, 1],
+                    [0, 0, 0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 0, 0, 1],
+                    [0, 0, 0, 0, 0, 0, 0],
+                ]
+            ),
+            "2_signal_2_width": np.array(
+                [
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [1, 0, 0, 0, 0, 0, 0],
+                    [1, 1, 0, 0, 0, 0, 0],
+                    [0, 1, 1, 0, 0, 0, 0],
+                    [1, 0, 1, 1, 0, 0, 0],
+                    [1, 1, 0, 1, 1, 0, 0],
+                    [0, 1, 1, 0, 1, 1, 0],
+                    [0, 0, 1, 1, 0, 1, 1],
+                    [0, 0, 0, 1, 1, 0, 1],
+                    [0, 0, 0, 0, 1, 1, 0],
+                    [0, 0, 0, 0, 0, 1, 1],
+                    [0, 0, 0, 0, 0, 0, 1],
+                    [0, 0, 0, 0, 0, 0, 0],
+                ]
+            ),
+        }
+        return SIGNALS[request.param]
+
+    @pytest.mark.parametrize(
+        "signal, bandwidth",
         [
-            [0, 0, 0, 0, 0, 0, 0],
-            [1, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 0, 0, 1],
-            [0, 0, 0, 0, 0, 0, 0],
-        ]
-    ),
-    "1_signal_2_width": np.array(
+            (
+                "1_signal_1_width",
+                1,
+            ),
+            (
+                "1_signal_2_width",
+                2,
+            ),
+        ],
+        indirect=["signal"],
+    )
+    def test_peak_widths(self, signal, bandwidth):
+
+        print(signal.shape)
+
+        ntime, nfreq = signal.shape[::-1][0:2]
+        print(ntime, nfreq)
+
+        times = np.arange(ntime)
+
+        peaks = xr.DataArray(
+            np.arange(1, ntime + 1), dims=["time"], coords={"time": times}, name="peaks"
+        ).expand_dims({"peak": [0]})
+
+        signal = xr.DataArray(
+            signal,
+            dims=["frequency", "time"],
+            coords={"time": times, "frequency": np.arange(nfreq)},
+        )
+
+        output = peak_widths(signal, peaks, dim="frequency")
+        widths = output[0]
+
+        assert len(output) == 4
+        assert len(widths.time) == ntime
+        assert peaks.ndim == widths.ndim
+        assert (bandwidth == widths).all()
+
+        assert "peak" in widths.dims
+        assert "peak" in widths.coords
+        assert "time" in widths.coords
+        assert "time" in widths.coords
+
+    @pytest.mark.parametrize(
+        "signal, bandwidth",
         [
-            [0, 0, 0, 0, 0, 0, 0],
-            [1, 0, 0, 0, 0, 0, 0],
-            [1, 1, 0, 0, 0, 0, 0],
-            [0, 1, 1, 0, 0, 0, 0],
-            [0, 0, 1, 1, 0, 0, 0],
-            [0, 0, 0, 1, 1, 0, 0],
-            [0, 0, 0, 0, 1, 1, 0],
-            [0, 0, 0, 0, 0, 1, 1],
-            [0, 0, 0, 0, 0, 0, 1],
-            [0, 0, 0, 0, 0, 0, 0],
-        ]
-    ),
-    "2_signal_1_width": np.array(
+            (
+                "2_signal_1_width",
+                1,
+            ),
+            (
+                "2_signal_2_width",
+                2,
+            ),
+        ],
+        indirect=["signal"],
+    )
+    def test_peak_width__multiple_peaks(self, signal, bandwidth):
+        """2D array that has more than one peak moving with time."""
+
+        print(signal.shape)
+        ntime, nfreq = signal.shape[::-1][0:2]
+        print(ntime, nfreq)
+
+        times = np.arange(ntime)
+
+        peaks = np.array(
+            [
+                np.arange(ntime) + 1,
+                np.arange(ntime) + 4,
+            ]
+        )
+
+        peaks = xr.DataArray(
+            peaks,
+            dims=["peak", "time"],
+            coords={"time": times, "peak": np.arange(2)},
+            name="peaks",
+        )
+
+        signal = xr.DataArray(
+            signal,
+            dims=["frequency", "time"],
+            coords={"time": times, "frequency": np.arange(nfreq)},
+        )
+
+        output = peak_widths(signal, peaks, dim="frequency")
+        widths = output[0]
+
+        assert len(output) == 4
+        assert len(widths.time) == ntime
+        assert peaks.ndim == widths.ndim
+        assert (bandwidth == widths).all()
+
+        assert "peak" in widths.dims
+        assert "peak" in widths.coords
+        assert "time" in widths.coords
+        assert "time" in widths.coords
+
+    @pytest.mark.parametrize(
+        "signal, tile, bandwidth",
         [
-            [0, 0, 0, 0, 0, 0, 0],
-            [1, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0],
-            [1, 0, 0, 1, 0, 0, 0],
-            [0, 1, 0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0, 1, 0],
-            [0, 0, 0, 1, 0, 0, 1],
-            [0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 0, 0, 1],
-            [0, 0, 0, 0, 0, 0, 0],
-        ]
-    ),
-    "2_signal_2_width": np.array(
-        [
-            [0, 0, 0, 0, 0, 0, 0],
-            [1, 0, 0, 0, 0, 0, 0],
-            [1, 1, 0, 0, 0, 0, 0],
-            [0, 1, 1, 0, 0, 0, 0],
-            [1, 0, 1, 1, 0, 0, 0],
-            [1, 1, 0, 1, 1, 0, 0],
-            [0, 1, 1, 0, 1, 1, 0],
-            [0, 0, 1, 1, 0, 1, 1],
-            [0, 0, 0, 1, 1, 0, 1],
-            [0, 0, 0, 0, 1, 1, 0],
-            [0, 0, 0, 0, 0, 1, 1],
-            [0, 0, 0, 0, 0, 0, 1],
-            [0, 0, 0, 0, 0, 0, 0],
-        ]
-    ),
-}
-
-
-@pytest.mark.parametrize(
-    "signal, bandwidth",
-    [
-        (
-            SIGNALS["1_signal_1_width"],
-            1,
-        ),
-        (
-            SIGNALS["1_signal_2_width"],
-            2,
-        ),
-    ],
-)
-def test_peak_widths(signal, bandwidth):
-
-    print(signal.shape)
-
-    ntime, nfreq = signal.shape[::-1][0:2]
-    print(ntime, nfreq)
-
-    times = np.arange(ntime)
-
-    peaks = xr.DataArray(
-        np.arange(1, ntime + 1), dims=["time"], coords={"time": times}, name="peaks"
-    ).expand_dims({"peak": [0]})
-
-    signal = xr.DataArray(
-        signal,
-        dims=["frequency", "time"],
-        coords={"time": times, "frequency": np.arange(nfreq)},
+            ("2_signal_1_width", (3, 1, 1), 1),
+            ("2_signal_2_width", (3, 1, 1), 2),
+        ],
+        indirect=["signal"],
     )
+    def test_peak_widths__multiple_peaks_3d(self, signal, tile, bandwidth):
+        """3D array that has more than one peak moving with time."""
+        signal = np.tile(signal, tile)
 
-    output = peak_widths(signal, peaks, dim="frequency")
-    widths = output[0]
+        print(signal.shape)
 
-    assert len(output) == 4
-    assert len(widths.time) == ntime
-    assert peaks.ndim == widths.ndim
-    assert (bandwidth == widths).all()
+        ntime, nfreq = signal.shape[::-1][0:2]
+        print(ntime, nfreq)
 
-    assert "peak" in widths.dims
-    assert "peak" in widths.coords
-    assert "time" in widths.coords
-    assert "time" in widths.coords
+        times = np.arange(ntime)
 
+        peaks = np.array(
+            [
+                np.arange(ntime) + 1,
+                np.arange(ntime) + 4,
+            ]
+        )
 
-@pytest.mark.parametrize(
-    "signal, bandwidth",
-    [
-        (
-            SIGNALS["2_signal_1_width"],
-            1,
-        ),
-        (
-            SIGNALS["2_signal_2_width"],
-            2,
-        ),
-    ],
-)
-def test_peak_width__multiple_peaks(signal, bandwidth):
-    """2D array that has more than one peak moving with time."""
+        extradim = np.arange(3)
 
-    print(signal.shape)
-    ntime, nfreq = signal.shape[::-1][0:2]
-    print(ntime, nfreq)
+        peaks = xr.DataArray(
+            peaks,
+            dims=["peak", "time"],
+            coords={"time": times, "peak": np.arange(2)},
+            name="peaks",
+        ).expand_dims({"extradim": extradim})
 
-    times = np.arange(ntime)
+        signal = xr.DataArray(
+            signal,
+            dims=["extradim", "frequency", "time"],
+            coords={"extradim": extradim, "time": times, "frequency": np.arange(nfreq)},
+        )
 
-    peaks = np.array(
-        [
-            np.arange(ntime) + 1,
-            np.arange(ntime) + 4,
-        ]
-    )
+        output = peak_widths(signal, peaks, dim="frequency")
+        widths = output[0]
 
-    peaks = xr.DataArray(
-        peaks,
-        dims=["peak", "time"],
-        coords={"time": times, "peak": np.arange(2)},
-        name="peaks",
-    )
+        assert len(output) == 4
+        assert len(widths.time) == ntime
+        # assert peaks.ndim == widths.ndim
+        print(peaks, widths)
+        print(bandwidth)
+        assert (bandwidth == widths).all()
 
-    signal = xr.DataArray(
-        signal,
-        dims=["frequency", "time"],
-        coords={"time": times, "frequency": np.arange(nfreq)},
-    )
-
-    output = peak_widths(signal, peaks, dim="frequency")
-    widths = output[0]
-
-    assert len(output) == 4
-    assert len(widths.time) == ntime
-    assert peaks.ndim == widths.ndim
-    assert (bandwidth == widths).all()
-
-    assert "peak" in widths.dims
-    assert "peak" in widths.coords
-    assert "time" in widths.coords
-    assert "time" in widths.coords
-
-
-@pytest.mark.parametrize(
-    "signal, bandwidth",
-    [
-        (np.tile(SIGNALS["2_signal_1_width"], (3, 1, 1)), 1),
-        (np.tile(SIGNALS["2_signal_2_width"], (3, 1, 1)), 2),
-    ],
-)
-def test_peak_widths__multiple_peaks_3d(signal, bandwidth):
-    """3D array that has more than one peak moving with time."""
-
-    print(signal.shape)
-
-    ntime, nfreq = signal.shape[::-1][0:2]
-    print(ntime, nfreq)
-
-    times = np.arange(ntime)
-
-    peaks = np.array(
-        [
-            np.arange(ntime) + 1,
-            np.arange(ntime) + 4,
-        ]
-    )
-
-    extradim = np.arange(3)
-
-    peaks = xr.DataArray(
-        peaks,
-        dims=["peak", "time"],
-        coords={"time": times, "peak": np.arange(2)},
-        name="peaks",
-    ).expand_dims({"extradim": extradim})
-
-    signal = xr.DataArray(
-        signal,
-        dims=["extradim", "frequency", "time"],
-        coords={"extradim": extradim, "time": times, "frequency": np.arange(nfreq)},
-    )
-
-    output = peak_widths(signal, peaks, dim="frequency")
-    widths = output[0]
-
-    assert len(output) == 4
-    assert len(widths.time) == ntime
-    # assert peaks.ndim == widths.ndim
-    print(peaks, widths)
-    print(bandwidth)
-    assert (bandwidth == widths).all()
-
-    assert "peak" in widths.dims
-    assert "peak" in widths.coords
-    assert "time" in widths.coords
-    assert "time" in widths.coords
+        assert "peak" in widths.dims
+        assert "peak" in widths.coords
+        assert "time" in widths.coords
+        assert "time" in widths.coords
 
 
 class TestFFT:
@@ -538,45 +541,43 @@ class TestFFTShift:
         assert "frequency" in shifted.dims
 
 
-def test_hilbert():
+class TestHilbert:
+    def test_hilbert(self):
+        duration = 10.0
+        fs = 8000.0
+        nsamples = int(fs * duration)
+        f = 100.0
+        A = 2.0
+        signal = A * np.sin(2.0 * np.pi * f * np.arange(nsamples) / fs)
+        signal = xr.DataArray(signal, dims=["time"])
+        result = hilbert(signal, dim="time")
+        assert np.allclose(np.abs(result).data, np.ones(nsamples) * A)
 
-    duration = 10.0
-    fs = 8000.0
-    nsamples = int(fs * duration)
-    f = 100.0
-    A = 2.0
-    signal = A * np.sin(2.0 * np.pi * f * np.arange(nsamples) / fs)
-    signal = xr.DataArray(signal, dims=["time"])
-    result = hilbert(signal, dim="time")
-    assert np.allclose(np.abs(result).data, np.ones(nsamples) * A)
+    def test_hilbert__N(self):
+        duration = 10.0
+        fs = 8000.0
+        nsamples = int(fs * duration)
+        f = 100.0
+        A = 2.0
+        N = 100
+        dim = "time"
+        signal = A * np.sin(2.0 * np.pi * f * np.arange(nsamples) / fs)
+        signal = xr.DataArray(signal, dims=[dim], coords={dim: np.arange(nsamples)})
+        result = hilbert(signal, N=N, dim=dim)
+        assert len(result) == N
+        assert dim in result.coords
 
+    def test_hilbert__N_without_coords(self):
 
-def test_hilbert__N():
-    duration = 10.0
-    fs = 8000.0
-    nsamples = int(fs * duration)
-    f = 100.0
-    A = 2.0
-    N = 100
-    dim = "time"
-    signal = A * np.sin(2.0 * np.pi * f * np.arange(nsamples) / fs)
-    signal = xr.DataArray(signal, dims=[dim], coords={dim: np.arange(nsamples)})
-    result = hilbert(signal, N=N, dim=dim)
-    assert len(result) == N
-    assert dim in result.coords
-
-
-def test_hilbert__N_without_coords():
-
-    duration = 10.0
-    fs = 8000.0
-    nsamples = int(fs * duration)
-    f = 100.0
-    A = 2.0
-    N = 100
-    dim = "time"
-    signal = A * np.sin(2.0 * np.pi * f * np.arange(nsamples) / fs)
-    signal = xr.DataArray(signal, dims=[dim])
-    result = hilbert(signal, N=N, dim=dim)
-    assert len(result) == N
-    assert dim not in result.coords
+        duration = 10.0
+        fs = 8000.0
+        nsamples = int(fs * duration)
+        f = 100.0
+        A = 2.0
+        N = 100
+        dim = "time"
+        signal = A * np.sin(2.0 * np.pi * f * np.arange(nsamples) / fs)
+        signal = xr.DataArray(signal, dims=[dim])
+        result = hilbert(signal, N=N, dim=dim)
+        assert len(result) == N
+        assert dim not in result.coords
